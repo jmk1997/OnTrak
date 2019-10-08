0da055507807c2984d80bb886d6aebc556589fa4
Antonio Garcia <antoniojg21@gmail.com>
2019-10-08 10:31:13 -0500

    Completed login page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './OnTrak_blue.png';
+import loginImg from './chi.jpg';
 import './App.css';
 
 class App extends Component {
 
   render() {
     return (
+    
       <div className="App" id="app-div">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <img src={loginImg} className="loginImage" />
+        <div className="loginForm">
+          <img src={logo} className="logo" />
+          <form>
+            <div className="formGroup">
+              <input type="email" name="email" id="email" placeholder="Email" required />
+            </div>
+
+            <div class="formGroup">
+              <input type="pass" name="pass" id="pass" placeholder="Password" required />
+            </div>
+
+            <div className="formSubmit">
+              <input type="submit" value="Login" class="submit" name="submit" id="submit" />
+            </div>
+          </form>
+        </div>
+          {/*
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          */}
+        
       </div>
+    
     );
   }
 }
