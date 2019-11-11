7150cc8cf143d8300358827bb8ead85bea95787b
Rafael Rejuso <r.rejuso@gmail.com>
2019-11-11 17:59:17 -0600

    Chat img

--- client/src/components/TertiaryNavbar.js
+++ client/src/components/TertiaryNavbar.js
@@ -12,6 +12,7 @@ import {
   Switch
 } from "react-router-dom";
 import UserContext from "../UserContext";
+import fakeChat from "../images/chat.png";
 
 import { withRouter } from "react-router";
 
@@ -182,6 +183,7 @@ class TertiaryNavbar extends React.Component {
                   render={() => (
                     <Container className="mx-0" fluid>
                       <h1>{this.props.groupName} - Chat</h1>
+                      <img src={fakeChat}></img>
                     </Container>
                   )}
                 />
